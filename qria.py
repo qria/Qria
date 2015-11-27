7f3325efb742872d33611f4aeed536f8c79a5b0d
qria <qria@qria.net>
2015-11-27 21:47:53 +0900

    Added send_message endpoint

--- qria.py
+++ qria.py
@@ -8,6 +8,13 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/api/message')
+def send_message():
+    message = request.args.get('message', None)
+    if not message:
+        return 'message is required!'
+    return 'message sent! %s' % message
+
 
 if __name__ == '__main__':
     app.run(debug=True)
